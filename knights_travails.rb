d77fe01554cdb410c9d40ab48febcf3505fe360d
Jonathan Lui <jonslui@gmail.com>
2020-04-27 16:56:37 -0400

    path too completion and number of moves now prints to console

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,8 +1,9 @@
 class Knight
-    attr_accessor :location, :children
+    attr_accessor :location, :children, :parent
     def initialize(location)
     @location = location
     @children = []
+    @parent = nil
     end
 end
 
@@ -14,8 +15,7 @@ class KnightsTravail
         @target_location = target_location
     end
 
-    def build_tree(current_queue = [self.start_location], level = 0)
-        level += 1
+    def build_tree(current_queue = [self.start_location])
         next_queue = []
             while current_queue.length > 0
                 current_position = current_queue[0].location
@@ -34,8 +34,10 @@ class KnightsTravail
                 tmp_queue = tmp_queue.select { |knight| knight.location[0] >= 1 && knight.location[0] <= 8 && knight.location[1] >= 1 && knight.location[1] <= 8 }
                 
                 tmp_queue.each do |knight|
+                    knight.parent = current_queue[0]
                     if knight.location == self.target_location
-                        return "FINISHED IN #{level} TURNS"
+                        path(knight)
+                        return
                     end
                     # add each child to the queue for the next function
                     next_queue << knight
@@ -45,16 +47,37 @@ class KnightsTravail
                 # remove the front of the queue
                 current_queue = current_queue.slice!(1..-1)
             end
-            build_tree(next_queue, level)
+            build_tree(next_queue)
+    end
+
+    def path(knight)
+        path_array = []
+        while knight.parent != nil
+            path_array.push(knight.location)
+            knight = knight.parent
+        end
+        path_array.push(knight.location)
+
+        # reverse the array so that the starting position is at the front of the array
+        path_array.reverse!
+
+        # print the first move and remove it from the array
+        print "Start:  #{path_array[0]}"
+        puts
+        path_array = path_array.slice(1..-1)
+
+        # print the remaining moves and count up with each one
+        path_array.each_with_index do |location, index|
+            print "Move #{index + 1}: #{location}"
+            puts
+        end
     end
 
 
 
 end
 
-kt_instance = KnightsTravail.new([1,1],[5,5])
-# puts kt_instance.start_location.location
-# puts kt_instance.target_location
-puts kt_instance.build_tree
-
-# puts kt_instance.start_location.children[0]
+# value one is where you want to get to
+# value two is where you want to go
+kt_instance = KnightsTravail.new([1,1],[1,8])
+kt_instance.build_tree
